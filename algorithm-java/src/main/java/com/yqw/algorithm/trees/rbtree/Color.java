c583ad8f0e5d6618a0309c7a1d9f2e93ec3df43b
Yang Qiwen <yangqiwen@pachiratech.com>
2019-09-24 16:15:44 +0800

    add rb tree

--- algorithm-java/src/main/java/com/yqw/algorithm/trees/rbtree/Color.java
+++
@@ -1,9 +0,0 @@
-package com.yqw.algorithm.trees.rbtree;
-
-/**
- * Created by iQiwen on 2019/6/18.
- */
-public enum Color {
-    RED,
-    BLACK
-}
--- algorithm-java/src/main/java/com/yqw/algorithm/trees/rbtree/RbRoot.java
+++
@@ -1,8 +0,0 @@
-package com.yqw.algorithm.trees.rbtree;
-
-/**
- * Created by iQiwen on 2019/6/18.
- */
-public class RbRoot {
-    public RbNode node;
-}
