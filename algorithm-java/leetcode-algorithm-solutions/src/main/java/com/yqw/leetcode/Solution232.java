c1c0905e897eacf1c1a71e2d31631c3a35f6712e
yang qiwen <yangqiwen@pachiratech.com>
2021-03-17 14:13:51 +0800

    add Sloution14

--- algorithm-java/leetcode-algorithm-solutions/src/main/java/com/yqw/leetcode/Solution232.java
+++ algorithm-java/leetcode-algorithm-solutions/src/main/java/com/yqw/leetcode/Solution232.java
@@ -26,20 +26,20 @@ public class Solution232 {
      * Removes the element from in front of queue and returns that element.
      */
     public int pop() {
-
+        return 0;
     }
 
     /**
      * Get the front element.
      */
     public int peek() {
-
+        return 0;
     }
 
     /**
      * Returns whether the queue is empty.
      */
     public boolean empty() {
-
+        return false;
     }
 }
