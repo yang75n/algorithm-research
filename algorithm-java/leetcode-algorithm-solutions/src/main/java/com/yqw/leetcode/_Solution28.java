966809ea97b96e5fe69c7581ca36ecd12eac533e
yang qiwen <yangqiwen@pachiratech.com>
2021-03-18 10:39:02 +0800

    updat

--- algorithm-java/leetcode-algorithm-solutions/src/main/java/com/yqw/leetcode/_Solution28.java
+++ algorithm-java/leetcode-algorithm-solutions/src/main/java/com/yqw/leetcode/_Solution28.java
@@ -1,11 +1,11 @@
 package com.yqw.leetcode;
 
 public class _Solution28 {
-    public int strStr(String haystack, String needle) {
 
+    public int strStr(String haystack, String needle) {
         return 0;
     }
-    
+
     public static void main(String[] args) {
         System.out.println(new _Solution28().strStr("", ""));
     }
